11072b6066b80545d953daaade7dd020696c501d
Lucas Stomberg <lstomber@epic.com>
2020-05-15 01:13:13 -0500

    Remove dependency on secondary library MZAppearance

---
+++ MZFormSheetPresentationController/Dummy.swift
@@ -0,0 +1,11 @@
+//
+//  Dummy.swift
+//  MZFormSheetPresentationController
+//
+//  Created by Lucas Stomberg on 5/15/20.
+//  Copyright © 2020 m1entus. All rights reserved.
+//
+
+import Foundation
+
+// This is here to force a swift module be created
